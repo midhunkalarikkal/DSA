9655a81a16cef718eda7675bc734d97084e91320
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-06-26 21:34:50 +0530

    Function added for insert , remove a node of specific index , converet the liked list to an array and remove a node with specific value

--- LinkedList/tempCodeRunnerFile.js
+++ LinkedList/tempCodeRunnerFile.js
@@ -23,7 +23,6 @@ class singlyLinkedList {
         const node = new Node(value)
         if(this.isEmpty()){
             this.head = node
-
         }else{
             node.next = this.head
             this.head = node
@@ -45,6 +44,81 @@ class singlyLinkedList {
         this.size++
     }
 
+    insert(value,index){
+        if(index < 0 || index >= this.size){
+            console.log("Invalid index!")
+            return false
+        }
+
+        if(index === 0 ){
+            this.prepend(value)
+        }else{
+            const node = new Node(value)
+            let prev = this.head
+            for(let i = 0; i < index -1; i++){
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+        }
+    }
+
+    removeFromIndex(index){
+        if(index < 0 || index >= this.size){
+            console.log("Invalid index")
+            return false
+        }
+
+        let removeNode;
+        if(index === 0){
+            removeNode = this.head
+            this.head = this.head.next
+        }else{
+            let prev = this.head
+            for(let i = 0; i < index-1; i++){
+                prev = prev.next
+            }
+            removeNode = prev.next
+            prev.next = removeNode.next
+        }
+        this.size--
+        console.log("removed node :",removeNode)
+    }
+
+    removeSpecificValue(value){
+        let removeNode;
+        let current = this.head
+        let previous = null
+
+        if(this.isEmpty()){
+            console.log("List is empty!")
+            return false
+        }
+
+        if(current !== null && current.value === value){
+            this.head = current.next
+            this.size--
+            console.log("Node removed : ",value)
+            return
+        }
+
+        while(current !== null && current.value !== value){
+            previous = current
+            current = current.next
+        }
+
+        if(current === null){
+            console.log("Value is not found in the list!")
+            return false
+        }
+
+        previous.next = current.next
+        this.size-
+        console.log("Node removed with value : ",value)
+        return
+    }
+
     traverse(){
         if(!this.isEmpty()){
             let current = this.head
@@ -57,6 +131,21 @@ class singlyLinkedList {
             console.log(listValues)
         }else{
             console.log("linked is empty.")
+        }
+    }
+
+    convertToArray(){
+        if(!this.isEmpty()){
+            let arr = []
+            let current = this.head
+            while(current){
+                arr.push(current.value)
+                current = current.next
+            }
+            console.log("Linked list converted to an array")
+            console.log(arr)
+        }else{
+            console.log("list is empty!")
         }
     }
 }
@@ -78,4 +167,11 @@ list.append(60)
 list.append(80)
 list.append(20)
 list.append(90)
+list.traverse()
+list.insert(888,4)
+list.traverse()
+list.removeFromIndex(5)
+list.traverse()
+list.convertToArray()
+list.removeSpecificValue(888)
 list.traverse()
