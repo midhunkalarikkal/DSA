c869ac3813884854e84f6369ed05f26ca572df50
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-06-27 02:31:53 +0530

    firstCharCap progeam done

--- Strings/tempCodeRunnerFile.js
+++ Strings/tempCodeRunnerFile.js
@@ -1,17 +1,5 @@
-function palindrome(str){
-    str = str.toLowerCase();
-    
-    let reverseStr = "";
-    
-    for (let i = str.length - 1; i >= 0; i--) {
-        reverseStr += str[i];
-    }
-    
-    if (str === reverseStr) {
-        return "It's a palindrome";
-    } else {
-        return "Not a palindrome";
-    }
+function firstCharCap(str){
+    return str.split(" ").map((word)=> word.charAt(0).toUpperCase() + word.slice(1).toLoweCase()).join("")
 }
 
-console.log(palindrome("MALAYALAM"))
+console.log("iam a software enginner")
