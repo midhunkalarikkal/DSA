c91d872dd82974351b059deef375ce82bc6b5943
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-06-27 03:56:40 +0530

    binary search ok

--- Search/Binary/tempCodeRunnerFile.js
+++ Search/Binary/tempCodeRunnerFile.js
@@ -1,25 +1,21 @@
-function firstOccurence(arr,target){
+function elementSearch(arr,target){
     arr = arr.sort((a,b) => a-b)
-    console.log("sorted array")
-    console.log(arr)
     let left = 0
     let right = arr.length - 1
-    let result = -1
 
     while( left <= right){
         let mid = Math.floor((left+right) / 2)
         if(arr[mid] === target){
-            result = mid
-            right = mid - 1
+            return mid
         }else if(arr[mid] < target){
             left = mid + 1
         }else{  
             right = mid - 1
         }
     }
-    return result
+    return -1
 }
 
-const arr = [1,33,4,66,75,83,48,99,,334,6,90,6633,57,89]
-const target = 33
-console.log(firstOccurence(arr,target))
+const arr = [1,33,4,66,75,83,48,99,4,6,90,6633,57,89]
+const target = 75
+console.log(elementSearch(arr,target))
