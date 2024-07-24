ef348be9358035c3beed6bcd4b9bbfad16e76e5d
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-07-24 20:16:42 +0530

    insertionSort tested

--- Sorting/InsertionSort/insertionSort.js
+++ Sorting/InsertionSort/insertionSort.js
@@ -1,15 +1,27 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let numberToInsert = arr[i]
-        let j = i - 1    
-        while(j >= 0 && arr[j] > numberToInsert){
+function insertionSort(arr){
+    let n = arr.length
+    let count = 0
+    for(let i = 1; i < n; i++){
+        let nToIn = arr[i]
+        let j = i -1
+        while(j >= 0 && arr[j] > nToIn){
             arr[j+1] = arr[j]
             j = j - 1
+            count ++
         }
-        arr[j+1] = numberToInsert
+        arr[j+1] = nToIn
     }
-    return arr
+    return {arr,count}
 }
-const arr = [8, 20, -2, 4, -6]
-insertionSort(arr)
-console.log(arr)
+
+const bestarr = [1,2,3,4,5,6,7,8,9]
+const avgarr = [6,3,5,1,9,2,8,4,7]
+const worstarr = [9,8,7,6,5,4,3,2,1]
+console.log("Best case")
+console.log(insertionSort(bestarr))
+
+console.log("Average case")
+console.log(insertionSort(avgarr))
+
+console.log("Worst case")
+console.log(insertionSort(worstarr))
--- Sorting/InsertionSort/tempCodeRunnerFile.js
+++ Sorting/InsertionSort/tempCodeRunnerFile.js
@@ -1,19 +1,27 @@
-function insertionSortDescending(arr){
-
-    if(arr.length <= 1 ){
-        return arr
-    }
-
-    for(i = 1; i < arr.length-1; i++){
-        let elementToInsert = arr[i]
-        let j = i - 1
-        while( j >= 0 && arr[j] < elementToInsert){
+function insertionSort(arr){
+    let n = arr.length
+    let count = 0
+    for(let i = 1; i < n; i++){
+        let nToIn = arr[i]
+        let j = i -1
+        while(j >= 0 && arr[j] > nToIn){
             arr[j+1] = arr[j]
-            j = j-1
+            j = j - 1
+            count ++
         }
-        arr[j+1] = elementToInsert
+        arr[j+1] = nToIn
     }
-    return arr
+    return {arr,count}
 }
-const arr = [8, 20, -2, 4, -6]
-console.log(insertionSortDescending(arr))
+
+const bestarr = [1,2,3,4,5,6,7,8,9]
+const avgarr = [6,3,5,1,9,2,8,4,7]
+const worstarr = [9,8,7,6,5,4,3,2,1]
+console.log("Best case")
+console.log(insertionSort(bestarr))
+
+console.log("Average case")
+console.log(insertionSort(avgarr))
+
+console.log("Worst case")
+console.log(insertionSort(worstarr))
