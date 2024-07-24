7132d2955b0b7999b804b2a9695fdd0e65e0712e
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-07-24 20:04:34 +0530

    Selction sort all cases tested

--- Sorting/SelectionSort/selectionSort1.js
+++ Sorting/SelectionSort/selectionSort1.js
@@ -1,21 +1,29 @@
-function selectionSort(arr) {
+function selectionSort(arr){
     let n = arr.length
-    for (let i = 0; i < n - 1; i++) {
+    let count = 0
+    for(let i = 0; i < n-1; i++){
         let minIndex = i
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j
+        for(let j = i+1; j < n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j 
             }
         }
-
-        if (minIndex !== i) {
-            let temp = arr[i]
-            arr[i] = arr[minIndex]
-            arr[minIndex] = temp
+        if(minIndex !== i){
+            [arr[i],arr[minIndex]] = [arr[minIndex],arr[i]]
+            count++
         }
     }
-    return arr
+    return {arr,count}
 }
 
-const arr = [1, 5, -45, 77, 2, 58, 93, -62, 89]
-console.log(selectionSort(arr))
+const bestarr = [1,2,3,4,5,6,7,8,9]
+const avgarr = [6,3,5,1,9,2,8,4,7]
+const worstarr = [9,8,7,6,5,4,3,2,1]
+console.log("Best case")
+console.log(selectionSort(bestarr))
+
+console.log("Average case")
+console.log(selectionSort(avgarr))
+
+console.log("Worst case")
+console.log(selectionSort(worstarr))
